2c913a930a1b046f0132f633d963b3717ed72180
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-10-05 12:12:55 -0300

    gg

--- index.php
+++ index.php
@@ -5,24 +5,27 @@
 <link rel="stylesheet" href="includes/lib/bootstrap-4.1.3-dist/css/bootstrap.css">
 
 <body>
+<img src="imagens/banner.png " width="1600" height="50"> 
 
 <div class="container-fluid">
         <div class="row justify-content-around gx-0">
-            <nav class="navbar bg-light">
+            <nav class="navbar bg-ligth">
+            <div class="col-12 col-lg-3  text-center">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="imagens/logo.jpeg" alt="exemplo de logo" width="125px">
+                    </a>
+                </div>
         
                 <div class="col-12 col-lg-6  text-center">
                     <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Sarch</button>
+                        <input class="form-control me-2" type="search" placeholder="Digite aqui " aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
                     </form>
                 </div>
         
                 <div class="col-6 col-lg-2  text-center">
                     <div class="btn-group dropend">
-                        <button type="button" class="btn btn-secondary">Menu</button>
-                        <button class="btn btn-secondary dropdown-toggle dropdown-toggle-split" type="button" id="dropdownMenuClickableOutside" 
-                        data-bs-toggle="dropdown" data-bs-auto-close="inside" aria-expanded="false">
-                            <span class="visually-hidden">Toggle Dropend</span>
+
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuClickableOutside">
                             <li><a class="dropdown-item" href="#">Filtros</a></li>
@@ -32,10 +35,15 @@
                     </div>
                 </div>
         
-              
+                <div class="col-6 col-lg-1  text-center">
+                    <a href="/Login/index.htm"><img src="imagens/login.png" alt="exemplo de login" width="50px"></a>
+                </div>
             </nav>
         </div>
     </div>
 
+    <img src="imagens/banner.png " width="1600" height="50"> 
+    <p style="text-align:center">Essa linha vai ficar centralizada.<br>
+E esta linha também.</p>
 </body>
 </html>
